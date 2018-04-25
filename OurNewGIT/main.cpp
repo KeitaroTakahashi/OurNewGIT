584822254f6e3b905135c7567e66520805162bef
Frédéric Dufeu <fredericdufeu@frederics-macbook-pro.local>
2018-04-25 16:58:50 +0100

    Initial Commit

---
+++ OurNewGIT/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  OurNewGIT
+//
+//  Created by Keitaro Takahashi on 2018/04/25.
+//  Copyright © 2018 Keitaro Takahashi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    
+    std::cout << "Hey this is Keitaro \n";
+    
+    
+    return 0;
+}
